36e84e9f2bf072f072662b555fafef613a13ceda
David Franco <david.d.franco28@gmail.com>
2026-01-31 18:17:29 -0800

    workflow complete

--- automation.py
+++ automation.py
@@ -1,5 +1,6 @@
 import functions
 
+#user workflow
 def main():
     functions.initialize()
     print("You may see who is due for an agreement signature, as well as who will be due in 90, 60, and 30 days respectively.")
@@ -7,35 +8,54 @@ def main():
     choice = input("Choices are as follows: a = expired, b = due in 30 days, c = due in 60 days, d = due in 90 days. All other input is invalid: ")
     choice = functions.input_check(choice)
     while(True):
+        #the user can now choose what subset of data they are interested in viewing
+        #as well as return to view another subset
         match choice.upper():
             case "A":
                 df = functions.employees_due()
-                print(df.head())
-                continue_working = input("Continue working? Enter a for yes, any other input will exit the program: ")
+                print(df.head(10))
+                export = input("Export table into Excel file? Enter a for yes, any other key will imply no.")
+                if export.upper() == "A":
+                    print("Exported! Note: you'll have to expand the columns to see the data properly")
+                    df.to_excel("output.xlsx")
+                continue_working = input("View other tables? Enter a for yes, any other input will exit the program: ")
                 if continue_working.upper() == "A":
                     continue
                 break
             case "B":
                 df = functions.thirty_days_out()
-                print(df.head())
-                continue_working = input("Continue working? Enter a for yes, any other input will exit the program: ")
+                print(df.head(10))
+                export = input("Export table into Excel file? Enter a for yes, any other key will imply no.")
+                if export.upper() == "A":
+                    print("Exported! Note: you'll have to expand the columns to see the data properly")
+                    df.to_excel("output.xlsx")
+                continue_working = input("View other tables? Enter a for yes, any other input will exit the program: ")
                 if continue_working.upper() == "A":
                     continue
                 break
             case "C":
                 df = functions.sixty_days_out()
-                print(df.head())
-                continue_working = input("Continue working? Enter a for yes, any other input will exit the program: ")
+                print(df.head(10))
+                export = input("Export table into Excel file? Enter a for yes, any other key will imply no.")
+                if export.upper() == "A":
+                    print("Exported! Note: you'll have to expand the columns to see the data properly")
+                    df.to_excel("output.xlsx")
+                continue_working = input("View other tables? Enter a for yes, any other input will exit the program: ")
                 if continue_working.upper() == "A":
                     continue
                 break
             case "D":
                 df = functions.ninety_days_out()
-                print(df.head())
-                continue_working = input("Continue working? Enter a for yes, any other input will exit the program: ")
+                print(df.head(10))
+                export = input("Export table into Excel file? Enter a for yes, any other key will imply no.")
+                if export.upper() == "A":
+                    print("Exported! Note: you'll have to expand the columns to see the data properly")
+                    df.to_excel("output.xlsx")
+                continue_working = input("View other tables? Enter a for yes, any other input will exit the program: ")
                 if continue_working.upper() == "A":
                     continue
                 break
+    print("Thank you for using!")
 
     
 
--- functions.py
+++ functions.py
@@ -12,32 +12,42 @@ def initialize():
             num += digit
         df.loc[i, "PERNR NUMBER"] = float(num)
     df["PERNR NUMBER"] = df["PERNR NUMBER"].astype(int)
+    #Basic data cleaning, in real life, PERNR numbers are all unique to the individual
+    df["DATE AGREEMENT SIGNED"] = pd.to_datetime(df["DATE AGREEMENT SIGNED"], format='mixed')
+    df.dropna(subset=["DATE AGREEMENT SIGNED"], inplace=True)
+    df.drop_duplicates(inplace=True)
 
 def input_check(user_input):
     while(not user_input.isalpha()):
         user_input = input("Please enter a valid response: ")
     return user_input
 
+#This function returns a dataframe subset of employees who are due for signature
 def employees_due():
-    today = datetime.today()
-    benchmark = timedelta(days = 365)
-    due = df.loc[["DATE AGREEMENT SIGNED" <= (today - benchmark) ]]
-    return due
+    benchmark = datetime.today() - timedelta(days = 365)
+    df2 = df[df["DATE AGREEMENT SIGNED"] < benchmark]
+    return df2
 
+#This function returns a dataframe subset of employees who are 90 days away from being due
 def ninety_days_out():
     today = datetime.today()
-    benchmark = timedelta(days = 365)
-    due = df.loc[["DATE AGREEMENT SIGNED" <= (today - benchmark) ]]
-    return due
+    upper_bound = today
+    lower_bound = today - timedelta(days = 90)
+    df2 = df[(df["DATE AGREEMENT SIGNED"] < upper_bound) & (df["DATE AGREEMENT SIGNED"] > lower_bound)]
+    return df2
 
+#This function returns a dataframe subset of employees who are 60 days away from being due
 def sixty_days_out():
     today = datetime.today()
-    benchmark = timedelta(days = 365)
-    due = df.loc[["DATE AGREEMENT SIGNED" <= (today - benchmark) ]]
-    return due
+    upper_bound = today
+    lower_bound = today - timedelta(days = 60)
+    df2 = df[(df["DATE AGREEMENT SIGNED"] < upper_bound) & (df["DATE AGREEMENT SIGNED"] > lower_bound)]
+    return df2
 
+#This function returns a dataframe subset of employees who are 30 days away from being due
 def thirty_days_out():
     today = datetime.today()
-    benchmark = timedelta(days = 365)
-    due = df.loc[["DATE AGREEMENT SIGNED" <= (today - benchmark) ]]
-    return due
+    upper_bound = today
+    lower_bound = today - timedelta(days = 30)
+    df2 = df[(df["DATE AGREEMENT SIGNED"] < upper_bound) & (df["DATE AGREEMENT SIGNED"] > lower_bound)]
+    return df2
